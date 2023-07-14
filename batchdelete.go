4b10ab8b7b4fc854602da027f0920309a06c8bd9
Harry <harry.luff4@gmail.com>
2023-07-14 12:01:28 +0100

    add survey checklist

--- batchdelete.go
+++ batchdelete.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+
+	survey "github.com/AlecAivazis/survey/v2"
 )
 
 func main() {
@@ -27,10 +29,48 @@ func main() {
 	branches := parseBranches(string(output))
 	if len(branches) == 0 {
 		fmt.Println("No branches found.")
-	} else {
-		fmt.Println("Branches:")
-		for _, branch := range branches {
-			fmt.Println(branch)
+		return
+	}
+
+	// Prepare the prompt for branch selection.
+	prompt := &survey.MultiSelect{
+		Message: "Select branches to delete:",
+		Options: branches,
+	}
+
+	// Prompt the user to select branches for deletion.
+	var branchSelection []string
+	if err := survey.AskOne(prompt, &branchSelection); err != nil {
+		log.Fatalf("Failed to read user input: %v", err)
+	}
+
+	if len(branchSelection) == 0 {
+		fmt.Println("No branches selected.")
+		return
+	}
+
+	// Confirm branch deletion.
+	confirm := false
+	confirmPrompt := &survey.Confirm{
+		Message: fmt.Sprintf("Are you sure you want to delete the selected branches: %s?", strings.Join(branchSelection, ", ")),
+	}
+	if err := survey.AskOne(confirmPrompt, &confirm); err != nil {
+		log.Fatalf("Failed to read user input: %v", err)
+	}
+
+	if !confirm {
+		fmt.Println("Deletion canceled.")
+		return
+	}
+
+	// Delete the selected branches.
+	for _, branch := range branchSelection {
+		cmd := exec.Command("git", "branch", "-D", branch)
+		cmd.Dir = dir
+		if err := cmd.Run(); err != nil {
+			log.Printf("Failed to delete branch '%s': %v", branch, err)
+		} else {
+			fmt.Printf("Deleted branch '%s'.\n", branch)
 		}
 	}
 }
